ac4ecc71fd8dc4b3fe8da1209cc652d528aa3456
Yenederr <166971498+Yenederr@users.noreply.github.com>
2024-04-14 02:35:24 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const yesBtn = document.querySelector('#yesBtn');
+
+yesBtn.addEventListener('click',function () {
+    alert('SABIA QUE IBAS A DECIR QUE SI MUAK 💕💕')
+    
+});
+
+const noBtn = document.querySelector('#noBtn');
+
+noBtn.addEventListener('mouseover', function () {
+    const randomX = parseInt(Math.random()*100);
+    const randomY = parseInt(Math.random()*100);
+    noBtn.style.setProperty('top',randomY+'%');
+    noBtn.style.setProperty('left',randomX+'%');
+    noBtn.style.setProperty('transform',`translate(-${randomX}%,-${randomY}%)`);
+})
